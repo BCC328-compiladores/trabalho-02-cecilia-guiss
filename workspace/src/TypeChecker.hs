d9dbe85d7dd78a02b341ad4a1edc92cbc0331f2d
GuilhermeSS <silvadesouzaguilherme24@gmail.com>
2026-02-07 22:10:22 -0300

    suporte a declaração de funções, parâmetros e tipos de retorno feito

--- workspace/src/TypeChecker.hs
+++ workspace/src/TypeChecker.hs
@@ -6,13 +6,24 @@ import qualified Data.Map as Map
 import Types
 import Parser
 
-data Env = Env { vars :: Map String Type }
+data Env = Env { vars :: Map String Type, context :: [Map String Type] }
 type CheckM a = ExceptT String (State Env) a
 
-checkProgram :: [Definition] -> Either String ()
-checkProgram _ = Right () -- Esqueleto inicial
+enterScope :: CheckM ()
+enterScope = modify (\s -> s { context = vars s : context s })
+
+exitScope :: CheckM ()
+exitScope = do
+    st <- get
+    case context st of
+        (old:rest) -> put st { vars = old, context = rest }
+        [] -> throwError "Escopo inválido"
 
 checkExpr :: Expr -> CheckM Type
 checkExpr (EInt _) = return TInt
-checkExpr (EFloat _) = return TFloat
+checkExpr (EVar n) = do
+    st <- get
+    case Map.lookup n (vars st) of
+        Just t -> return t
+        Nothing -> throwError "Variável não declarada"
 checkExpr _ = throwError "Não implementado"
---
+++ workspace/test/fail_type.sl
@@ -0,0 +1,5 @@
+func main() {
+    let x : int = 10;
+    let y : bool = true;
+    let z : int = x + y; // Error
+}
